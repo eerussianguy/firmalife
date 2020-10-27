2a8666d6d9b8ea2c9bdddbccf8f2ddaa6d2112c0
EERussianguy <eerussianguy@gmail.com>
2020-10-26 20:37:02 -0400

    rewrote leaf decay for my "tree" to work. pretty inefficient but like, who cares

--- src/main/java/com/eerussianguy/firmalife/blocks/BlockCinnamonLeaves.java
+++ src/main/java/com/eerussianguy/firmalife/blocks/BlockCinnamonLeaves.java
@@ -43,14 +43,23 @@ public class BlockCinnamonLeaves extends BlockLeavesTFC
     @Override
     public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
     {
-        this.doLeafDecay(worldIn, pos, state);
+        worldIn.scheduleUpdate(pos, state.getBlock(), 1);
     }
 
     @Override
     @SuppressWarnings("deprecation")
     public void neighborChanged(IBlockState state, World world, BlockPos pos, @Nullable Block blockIn, @Nullable BlockPos fromPos)
     {
-        world.scheduleUpdate(pos, this, 0);
+        for (EnumFacing d : EnumFacing.VALUES)
+        {
+            for (int i = 1; i < 4; i++)
+            {
+                Block offsetBlock = world.getBlockState(pos.offset(d, i)).getBlock();
+                if (offsetBlock instanceof BlockCinnamonLog)
+                    return;
+            }
+        }
+        world.setBlockToAir(pos);
     }
 
     @Override
@@ -59,63 +68,4 @@ public class BlockCinnamonLeaves extends BlockLeavesTFC
     {
         return ImmutableList.of(ItemStack.EMPTY);
     }
-
-    private void doLeafDecay(World world, BlockPos pos, IBlockState state)
-    {
-        // TFC Leaf Decay but with the conditions changed to match mine.
-        if (world.isRemote || !state.getValue(DECAYABLE))
-            return;
-
-        Set<BlockPos> paths = new HashSet<>();
-        Set<BlockPos> evaluated = new HashSet<>(); // Leaves that everything was evaluated so no need to do it again
-        List<BlockPos> pathsToAdd; // New Leaves that needs evaluation
-        BlockPos.MutableBlockPos pos1 = new BlockPos.MutableBlockPos(pos);
-        IBlockState state1;
-        paths.add(pos); // Center block
-
-        for (int i = 0; i < 4; i++)
-        {
-            pathsToAdd = new ArrayList<>();
-            for (BlockPos p1 : paths)
-            {
-                for (EnumFacing face : EnumFacing.values())
-                {
-                    pos1.setPos(p1).move(face);
-                    if (evaluated.contains(pos1) || !world.isBlockLoaded(pos1))
-                        continue;
-                    state1 = world.getBlockState(pos1);
-                    if (state1.getBlock() instanceof BlockCinnamonLog)
-                        return;
-                    if (state1.getBlock() instanceof BlockCinnamonLeaves)
-                        pathsToAdd.add(pos1.toImmutable());
-                }
-                evaluated.add(p1); // Evaluated
-            }
-            paths.addAll(pathsToAdd);
-            paths.removeAll(evaluated);
-        }
-
-        world.setBlockToAir(pos);
-        int particleScale = 10;
-        double x = pos.getX();
-        double y = pos.getY();
-        double z = pos.getZ();
-        for (int i = 1; i < RNG.nextInt(4); i++)
-        {
-            switch (RNG.nextInt(4))
-            {
-                case 1:
-                    TFCParticles.LEAF1.sendToAllNear(world, x + RNG.nextFloat() / particleScale, y - RNG.nextFloat() / particleScale, z + RNG.nextFloat() / particleScale, (RNG.nextFloat() - 0.5) / particleScale, -0.15D + RNG.nextFloat() / particleScale, (RNG.nextFloat() - 0.5) / particleScale, 90);
-                    break;
-                case 2:
-                    TFCParticles.LEAF2.sendToAllNear(world, x + RNG.nextFloat() / particleScale, y - RNG.nextFloat() / particleScale, z + RNG.nextFloat() / particleScale, (RNG.nextFloat() - 0.5) / particleScale, -0.15D + RNG.nextFloat() / particleScale, (RNG.nextFloat() - 0.5) / particleScale, 70);
-                    break;
-                case 3:
-                    TFCParticles.LEAF3.sendToAllNear(world, x + RNG.nextFloat() / particleScale, y - RNG.nextFloat() / particleScale, z + RNG.nextFloat() / particleScale, (RNG.nextFloat() - 0.5) / particleScale, -0.15D + RNG.nextFloat() / particleScale, (RNG.nextFloat() - 0.5) / particleScale, 80);
-                    break;
-            }
-        }
-    }
-
-
 }
