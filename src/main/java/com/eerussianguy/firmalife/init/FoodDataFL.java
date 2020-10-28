e80c376d3899674811b92210ecc9985a521fde2e
Indib <IndiBowstring@gmail.com>
2020-10-28 11:41:06 -0700

    FoodDataFL Dried Foods use defaults

--- src/main/java/com/eerussianguy/firmalife/init/FoodDataFL.java
+++ src/main/java/com/eerussianguy/firmalife/init/FoodDataFL.java
@@ -6,26 +6,9 @@ public class FoodDataFL
 {
     public static final FoodData CHOCOLATE = new FoodData(4, 0.0F, 0.2F, 0.0F, 0.0F, 0.0F, 0.0F, 0.2F, 1.1F);
     public static final FoodData COCOA_BEANS = new FoodData(4, 0.0F, 0.1F, 0.1F, 0.0F, 0.0F, 0.1F, 0.0F, 0.5F);
-    //TODO: balance dried fruits (currently apply a 1/3 modifier to decay rate)
-    public static final FoodData DRIED_BANANA = new FoodData(4, 0.0F, 0.2F, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.66F);
-    public static final FoodData DRIED_BLACKBERRY = new FoodData(4, 0.0F, 0.2F, 0.0F, 0.75F, 0.0F, 0.0F, 0.0F, 1.63F);
-    public static final FoodData DRIED_BLUEBERRY = new FoodData(4, 0.0F, 0.2F, 0.0F, 0.75F, 0.0F, 0.0F, 0.0F, 1.63F);
-    public static final FoodData DRIED_BUNCH_BERRY = new FoodData(4, 0.0F, 0.5F, 0.0F, 0.75F, 0.0F, 0.0F, 0.0F, 1.63F);
-    public static final FoodData DRIED_CHERRY = new FoodData(4, 0.0F, 0.2F, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 1.33F);
-    public static final FoodData DRIED_CLOUD_BERRY = new FoodData(4, 0.0F, 0.5F, 0.0F, 0.75F, 0.0F, 0.0F, 0.0F, 1.63F);
     public static final FoodData DRIED_COCOA_BEANS = new FoodData(4, 0.0F, 0.1F, 0.1F, 0.0F, 0.0F, 0.1F, 0.0F, 0.5F);
-    public static final FoodData DRIED_CRANBERRY = new FoodData(4, 0.0F, 0.2F, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.6F);
-    public static final FoodData DRIED_ELDERBERRY = new FoodData(4, 0.0F, 0.2F, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 1.63F);
-    public static final FoodData DRIED_GOOSEBERRY = new FoodData(4, 0.0F, 0.5F, 0.0F, 0.75F, 0.0F, 0.0F, 0.0F, 1.63F);
-    public static final FoodData DRIED_GREEN_APPLE = new FoodData(4, 0.0F, 0.5F, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.83F);
-    public static final FoodData DRIED_LEMON = new FoodData(4, 0.0F, 0.2F, 0.0F, 0.75F, 0.0F, 0.0F, 0.0F, 0.66F);
-    public static final FoodData DRIED_OLIVE = new FoodData(4, 0.0F, 0.2F, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.53F);
-    public static final FoodData DRIED_ORANGE = new FoodData(4, 0.0F, 0.5F, 0.0F, 0.5F, 0.0F, 0.0F, 0.0F, 0.73F);
-    public static final FoodData DRIED_PEACH = new FoodData(4, 0.0F, 0.5F, 0.0F, 0.5F, 0.0F, 0.0F, 0.0F, 0.93F);
-    public static final FoodData DRIED_PLUM = new FoodData(4, 0.0F, 0.5F, 0.0F, 0.75F, 0.0F, 0.0F, 0.0F, 0.93F);
-    public static final FoodData DRIED_RASPBERRY = new FoodData(4, 0.0F, 0.5F, 0.0F, 0.75F, 0.0F, 0.0F, 0.0F, 1.63F);
-    public static final FoodData DRIED_RED_APPLE = new FoodData(4, 0.0F, 0.5F, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.56F);
-    public static final FoodData DRIED_SNOW_BERRY = new FoodData(4, 0.0F, 0.2F, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 1.63F);
-    public static final FoodData DRIED_STRAWBERRY = new FoodData(4, 0.0F, 0.5F, 0.0F, 0.5F, 0.0F, 0.0F, 0.0F, 1.63F);
-    public static final FoodData DRIED_WINTERGREEN_BERRY = new FoodData(4, 0.0F, 0.2F, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 1.63F);
+
+    public static final FoodData DRIED_FRUIT_SATURATION = new FoodData(4,0F,0.5F,0F,0.5F,0F,0F,0F,1.6F);
+    public static final FoodData DRIED_FRUIT_DECAY = new FoodData(4,0F,0.2F,0F,0.5F,0F,0F,0F,0.8F);
+    public static final FoodData DRIED_FRUIT_CATEGORY = new FoodData(4,0F,0.2F,0F,1F,0F,0F,0F,1.6F);
 }
