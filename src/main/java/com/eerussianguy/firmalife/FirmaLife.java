8392f9abde744b920b78564dfc83b1e9f11cb6d6
EERussianguy <eerussianguy@gmail.com>
2021-03-04 20:10:27 -0500

    push new version

--- src/main/java/com/eerussianguy/firmalife/FirmaLife.java
+++ src/main/java/com/eerussianguy/firmalife/FirmaLife.java
@@ -20,7 +20,7 @@ public class FirmaLife
 {
     public static final String MOD_ID = "firmalife";
     public static final String MODNAME = "FirmaLife";
-    public static final String MODVERSION = "0.0.1";
+    public static final String MODVERSION = "0.1.0";
 
     @Mod.Instance
     private static FirmaLife INSTANCE = null;
