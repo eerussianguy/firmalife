9345f5c117218a0738f1b14518da1831273ac6c2
EERussianguy <eerussianguy@gmail.com>
2021-03-07 14:05:26 -0500

    bump version

--- src/main/java/com/eerussianguy/firmalife/FirmaLife.java
+++ src/main/java/com/eerussianguy/firmalife/FirmaLife.java
@@ -20,7 +20,7 @@ public class FirmaLife
 {
     public static final String MOD_ID = "firmalife";
     public static final String MODNAME = "FirmaLife";
-    public static final String MODVERSION = "0.1.0";
+    public static final String MODVERSION = "0.1.1";
 
     @Mod.Instance
     private static FirmaLife INSTANCE = null;
