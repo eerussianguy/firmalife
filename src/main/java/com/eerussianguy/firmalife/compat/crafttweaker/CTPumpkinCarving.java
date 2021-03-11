2c686dbbd3e3913190e53a161c1c0e9def406c5c
Mrthomas20121 <thomas77300@gmail.com>
2021-03-11 17:13:52 +0100

    Crafttweaker Pumpkin Carving

---
+++ src/main/java/com/eerussianguy/firmalife/compat/crafttweaker/CTPumpkinCarving.java
@@ -0,0 +1,103 @@
+package com.eerussianguy.firmalife.compat.crafttweaker;
+
+import com.blamejared.mtlib.helpers.InputHelper;
+import com.eerussianguy.firmalife.init.KnappingFL;
+import com.eerussianguy.firmalife.recipe.KnappingRecipeFood;
+import crafttweaker.CraftTweakerAPI;
+import crafttweaker.IAction;
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.item.IItemStack;
+import net.dries007.tfc.api.recipes.knapping.KnappingRecipe;
+import net.dries007.tfc.api.registries.TFCRegistries;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.registries.IForgeRegistryModifiable;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+import java.util.ArrayList;
+
+@ZenClass("mods.firmalife.PumpkinCarving")
+@ZenRegister
+public class CTPumpkinCarving {
+
+    @ZenMethod
+    public static void addRecipe(String recipe_name, IItemStack output, String... pattern)
+    {
+        if (output == null || pattern.length < 1 || pattern.length > 5)
+            throw new IllegalArgumentException("Output item must be non-null and pattern must be a closed interval [1, 5]");
+        ItemStack outputStack = InputHelper.toStack(output);
+        KnappingRecipe recipe = new KnappingRecipeFood(KnappingFL.PUMPKIN, true, outputStack, pattern).setRegistryName(recipe_name);
+        CraftTweakerAPI.apply(new IAction()
+        {
+            @Override
+            public void apply()
+            {
+                TFCRegistries.KNAPPING.register(recipe);
+            }
+
+            @Override
+            public String describe()
+            {
+                //noinspection ConstantConditions
+                return "Adding Pumpkin Carving recipe " + recipe.getRegistryName().toString();
+            }
+        });
+    }
+
+    @ZenMethod
+    public static void removeRecipe(IItemStack output)
+    {
+        if (output == null) throw new IllegalArgumentException("Output not allowed to be empty");
+        ItemStack item = (ItemStack) output.getInternal();
+        ArrayList<KnappingRecipe> removeList = new ArrayList<>();
+        TFCRegistries.KNAPPING.getValuesCollection()
+                .stream()
+                .filter(x -> x.getType() == KnappingFL.PUMPKIN && x.getOutput(ItemStack.EMPTY).isItemEqual(item))
+                .forEach(removeList::add);
+        for (KnappingRecipe rem : removeList)
+        {
+            CraftTweakerAPI.apply(new IAction()
+            {
+                @Override
+                public void apply()
+                {
+                    IForgeRegistryModifiable modRegistry = (IForgeRegistryModifiable) TFCRegistries.KNAPPING;
+                    modRegistry.remove(rem.getRegistryName());
+                }
+
+                @Override
+                public String describe()
+                {
+                    //noinspection ConstantConditions
+                    return "Removing Pumpking Carving recipe " + rem.getRegistryName().toString();
+                }
+            });
+        }
+    }
+
+    @ZenMethod
+    public static void removeRecipe(String registryName)
+    {
+        KnappingRecipe recipe = TFCRegistries.KNAPPING.getValue(new ResourceLocation(registryName));
+        if (recipe != null)
+        {
+            CraftTweakerAPI.apply(new IAction()
+            {
+                @Override
+                public void apply()
+                {
+                    IForgeRegistryModifiable modRegistry = (IForgeRegistryModifiable) TFCRegistries.KNAPPING;
+                    modRegistry.remove(recipe.getRegistryName());
+                }
+
+                @Override
+                public String describe()
+                {
+                    //noinspection ConstantConditions
+                    return "Removing Pumpking Carving recipe " + recipe.getRegistryName().toString();
+                }
+            });
+        }
+    }
+}
