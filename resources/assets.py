2fe14898d94fcfef7c7fe87baafc955b20f8d3c2
Charnuz <charnuz.sc2@gmail.com>
2025-11-10 17:59:02 -0600

    fix potted plant models

--- resources/assets.py
+++ resources/assets.py
@@ -527,7 +527,7 @@ def contained_fluid(rm: ResourceManager, name_parts: utils.ResourceIdentifier, b
 
 def flower_pot_cross(rm: ResourceManager, simple_name: str, name: str, model: str, texture: str, loot: str, tinted: bool = False):
     rm.blockstate(name, model='firmalife:block/%s' % model).with_lang(lang('potted %s', simple_name))
-    rm.block_model(model, parent='minecraft:block/tinted_flower_pot_cross' if tinted else 'minecraft:block/flower_pot_cross', textures={'plant': texture, 'dirt': 'tfc:block/dirt/loam'})
+    rm.block_model(model, parent='minecraft:block/tinted_flower_pot_cross' if tinted else 'minecraft:block/flower_pot_cross', textures={'plant': texture, 'dirt': 'tfc:block/dirt/entisol'})
 
 
 def simple_plant_data(rm: ResourceManager, p: str, bees: bool = True, straw: bool = True):
