85d4b99c3510da85607c838c603836b62f727e27
Charnuz <charnuz.sc2@gmail.com>
2025-11-12 22:30:26 -0600

    improve wine shelf item transfer and shape

--- src/main/java/com/eerussianguy/firmalife/common/blocks/WineShelfBlock.java
+++ src/main/java/com/eerussianguy/firmalife/common/blocks/WineShelfBlock.java
@@ -1,9 +1,11 @@
 package com.eerussianguy.firmalife.common.blocks;
 
+import com.eerussianguy.firmalife.FirmaLife;
 import com.eerussianguy.firmalife.common.FLHelpers;
 import com.eerussianguy.firmalife.common.FLTags;
 import com.eerussianguy.firmalife.common.blockentities.FLBlockEntities;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.ItemInteractionResult;
@@ -13,6 +15,7 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -24,7 +27,14 @@ import net.dries007.tfc.util.Helpers;
 public class WineShelfBlock extends FourWayDeviceBlock
 {
     public static final VoxelShape[] SHAPES = Helpers.computeHorizontalShapes(dir -> Shapes.or(
-        Shapes.join(Shapes.block(), Helpers.rotateShape(dir, 1, 1, 0, 15, 15, 15), BooleanOp.ONLY_FIRST)
+        Shapes.join(
+            Shapes.block(),
+            Helpers.rotateShape(dir, 1, 1, 0, 15, 15, 15), BooleanOp.ONLY_FIRST
+        ),
+        // Middle shelf
+        box(0, 7, 0, 16, 9, 16),
+        // Vertical seperator
+        box(7, 0, 1, 9, 16, 16)
     ));
 
     public WineShelfBlock(ExtendedProperties properties)
@@ -36,15 +46,29 @@ public class WineShelfBlock extends FourWayDeviceBlock
     public ItemInteractionResult useItemOn(ItemStack held, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result)
     {
         return FLHelpers.consumeItemInventory(level, pos, FLBlockEntities.WINE_SHELF, (shelf, inv) -> {
+            int slot = getSlotFromPos(state, result.getLocation().subtract(pos.getX(), pos.getY(), pos.getZ()));
+            ItemStack slotItem = inv.getStackInSlot(slot);
             if (Helpers.isItem(held, FLTags.Items.WINE_BOTTLES))
             {
                 Helpers.playSound(level, pos, SoundEvents.GLASS_PLACE);
-                return FLHelpers.insertOneAny(level, held, 0, 3, shelf, player);
+                if (slotItem.isEmpty())
+                {
+                    //Insert
+                    return FLHelpers.insertOne(level, held, slot, inv, player);
+                }
+                else
+                {
+                    //Swap with hand
+                    //TODO this could be optimized
+                    FLHelpers.takeOne(level, slot, inv, player);
+                    return FLHelpers.insertOne(level, held, slot, inv, player);
+                }
             }
-            else if (held.isEmpty())
+            else if (held.isEmpty() && !slotItem.isEmpty())
             {
+                //Extract
                 Helpers.playSound(level, pos, SoundEvents.GLASS_PLACE);
-                return FLHelpers.takeOneAny(level, 0, 3, shelf, player);
+                return FLHelpers.takeOne(level, slot, inv, player);
             }
             return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
         });
@@ -66,5 +90,20 @@ public class WineShelfBlock extends FourWayDeviceBlock
     public float getShadeBrightness(BlockState state, BlockGetter level, BlockPos pos)
     {
         return 1f;
+    }
+
+    private int getSlotFromPos(BlockState state, Vec3 pos)
+    {
+        int slot = 0;
+        if ((state.getValue(FACING).getAxis().equals(Direction.Axis.Z) ? pos.x : pos.z) < .5f)
+        {
+            slot += 2;
+        }
+        if (pos.y < 0.5f)
+        {
+            slot += 1;
+        }
+        FirmaLife.LOGGER.info("point {} slot {}", pos, slot);
+        return slot;
     }
 }
