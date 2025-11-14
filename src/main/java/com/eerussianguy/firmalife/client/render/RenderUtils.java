2fa584d40e200edbd3dfb31e885f6e639afae166
Charnuz <charnuz.sc2@gmail.com>
2025-11-13 18:19:52 -0600

    change UV to render bottom to top

--- src/main/java/com/eerussianguy/firmalife/client/render/RenderUtils.java
+++ src/main/java/com/eerussianguy/firmalife/client/render/RenderUtils.java
@@ -26,15 +26,15 @@ public final class RenderUtils
     public static float[][] getCrossHalfVertices(float x1, float y1, float z1, float x2, float y2, float z2, float u1, float v1, float u2, float v2)
     {
         return new float[][] {
-            {x1, y1, z1, u1, v1},
-            {x2, y1, z2, u2, v1},
-            {x2, y2, z2, u2, v2},
-            {x1, y2, z1, u1, v2},
+            {x1, y1, z1, u2, v2},
+            {x2, y1, z2, u1, v2},
+            {x2, y2, z2, u1, v1},
+            {x1, y2, z1, u2, v1},
 
-            {x2, y1, z2, u2, v1},
-            {x1, y1, z1, u1, v1},
-            {x1, y2, z1, u1, v2},
-            {x2, y2, z2, u2, v2}
+            {x2, y1, z2, u1, v2},
+            {x1, y1, z1, u2, v2},
+            {x1, y2, z1, u2, v1},
+            {x2, y2, z2, u1, v1}
         };
     }
 }
