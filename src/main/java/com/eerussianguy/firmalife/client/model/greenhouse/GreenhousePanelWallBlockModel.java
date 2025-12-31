2af3cc56bb4529e3593cfecefe88540ff685a0d5
Charnuz <charnuz.sc2@gmail.com>
2025-12-31 15:17:36 -0600

    correct normals and most of the UVs

--- src/main/java/com/eerussianguy/firmalife/client/model/greenhouse/GreenhousePanelWallBlockModel.java
+++ src/main/java/com/eerussianguy/firmalife/client/model/greenhouse/GreenhousePanelWallBlockModel.java
@@ -64,8 +64,9 @@ public class GreenhousePanelWallBlockModel extends GreenhouseBlockModel.Baked
         //TODO normals are incorrect for RenderHelpers#renderTexturedCuboid calls for rotated models
         Vec3i normal = switch (facing)
         {
-            case NORTH -> new Vec3i(1, 0, 0);
-            default -> new Vec3i(-1, 0, 0);
+            case NORTH, SOUTH -> new Vec3i(1, 0, 0);
+            case EAST, WEST -> new Vec3i(0, 0, 1);
+            default -> new Vec3i(0, 0, 0);
         };
 
         poseStack.pushPose();
@@ -80,12 +81,12 @@ public class GreenhousePanelWallBlockModel extends GreenhouseBlockModel.Baked
         // Top
         if (up == GreenhouseConnectable.PostType.BOTH || up == side.toPost().opposite())
         {
-            RenderHelpers.renderTexturedCuboid(poseStack, buffer, postTexture, packedLight, packedOverlay, 0, 14 / 16f, 0f, 1, 1f, 2 / 16f);
+            drawCube(poseStack, buffer, postTexture, packedLight, packedOverlay, 0, 14 / 16f, 0f, 1, 1f, 2 / 16f, 0, 0, 1, 1, normal);
         }
         // Bottom
         if (!down)
         {
-            RenderHelpers.renderTexturedCuboid(poseStack, buffer, postTexture, packedLight, packedOverlay, 0, 0, 0f, 1, 2 / 16f, 2 / 16f);
+            drawCube(poseStack, buffer, postTexture, packedLight, packedOverlay, 0, 0, 0f, 1, 2 / 16f, 2 / 16f, 0, 0, 1, 1, normal);
         }
 
         poseStack.popPose();
@@ -104,30 +105,30 @@ public class GreenhousePanelWallBlockModel extends GreenhouseBlockModel.Baked
 
         if (corner)
         {
-            RenderHelpers.renderTexturedCuboid(poseStack, buffer, postTexture, packedLight, packedOverlay, 14 / 16f, postStart, 0f, 1f, postEnd, 2 / 16f);
-            RenderHelpers.renderTexturedCuboid(poseStack, buffer, postTexture, packedLight, packedOverlay, 14 / 16f, postStart, width, 1f, postEnd, 1);
+            drawCube(poseStack, buffer, postTexture, packedLight, packedOverlay, 14 / 16f, postStart, 0f, 1f, postEnd, 2 / 16f, 0, 0, 1, 1, normal);
+            drawCube(poseStack, buffer, postTexture, packedLight, packedOverlay, 14 / 16f, postStart, width, 1f, postEnd, 1, 0, 0, 1, 1, normal);
             // Bottom post for the extra wall
             if (!bottom)
             {
-                RenderHelpers.renderTexturedCuboid(poseStack, buffer, postTexture, packedLight, packedOverlay, 14 / 16f, 0, 2 / 16f, 1f, 2 / 16f, 1);
+                drawCube(poseStack, buffer, postTexture, packedLight, packedOverlay, 14 / 16f, 0, 2 / 16f, 1f, 2 / 16f, 1, 0, 0, 1, 1, normal);
             }
             // Top Post for the extra wall
             if (!topConnection)
             {
-                RenderHelpers.renderTexturedCuboid(poseStack, buffer, postTexture, packedLight, packedOverlay, 14 / 16f, 14 / 16f, 0, 1f, 1, 1);
+                drawCube(poseStack, buffer, postTexture, packedLight, packedOverlay, 14 / 16f, 14 / 16f, 0, 1f, 1, 1, 0, 0, 1, 1, normal);
             }
 
             // Glass panels connected to the corner post, and cannot have side connections
-            drawGlass(poseStack, buffer, packedLight, packedOverlay, false, GreenhouseConnectable.PostType.LEFT.contains(top), bottom, getXVertices(0.5f, topOffset, 1 / 16f, 14 / 16f, downOffset, 1 / 16f, 2 / 16f, topOffset, 0.5f, downOffset), normal);
-            drawGlass(poseStack, buffer, packedLight, packedOverlay, false, topConnection, bottom, getXVertices(15 / 16f, 0, 0, 15 / 16f, 1, 0.5f, 0.5f, 0, 1, 1), normal);
+            drawGlass(poseStack, buffer, packedLight, packedOverlay, false, GreenhouseConnectable.PostType.LEFT.contains(top), bottom, getPlaneVertices(0.5f, topOffset, 1 / 16f, 14 / 16f, downOffset, 1 / 16f, 2 / 16f, topOffset, 0.5f, downOffset), normal);
+            drawGlass(poseStack, buffer, packedLight, packedOverlay, false, topConnection, bottom, getPlaneVertices(15 / 16f, 0, 0, 15 / 16f, 1, 0.5f, 0.5f, 0, 1, 1), normal);
 
             // Glass panel in the extra wall not connected to the corner post
-            drawGlass(poseStack, buffer, packedLight, packedOverlay, side, topConnection, bottom, getXVertices(15 / 16f, 0, 0.5f, 15 / 16f, 1, 1, 0, 0, 0.5f, 1), normal);
+            drawGlass(poseStack, buffer, packedLight, packedOverlay, side, topConnection, bottom, getPlaneVertices(15 / 16f, 0, 0.5f, 15 / 16f, 1, 1, 0, 0, 0.5f, 1), normal);
         }
         else
         {
-            drawGlass(poseStack, buffer, packedLight, packedOverlay, side, topConnection, bottom, getXVertices(0.5f, topOffset, 1 / 16f, width, downOffset, 1 / 16f, 1 - width, topOffset, 0.5f, downOffset), normal);
-            RenderHelpers.renderTexturedCuboid(poseStack, buffer, postTexture, packedLight, packedOverlay, width, postStart, 0f, 1f, postEnd, 2 / 16f);
+            drawGlass(poseStack, buffer, packedLight, packedOverlay, side, topConnection, bottom, getPlaneVertices(0.5f, topOffset, 1 / 16f, width, downOffset, 1 / 16f, 1 - width, topOffset, 0.5f, downOffset), normal);
+            drawCube(poseStack, buffer, postTexture, packedLight, packedOverlay, width, postStart, 0f, 1f, postEnd, 2 / 16f, 0, 0, 1, 1, normal);
         }
     }
 
@@ -142,20 +143,27 @@ public class GreenhousePanelWallBlockModel extends GreenhouseBlockModel.Baked
         float downOffset = (true ? 0 : 2) / 16f;
         if (corner)
         {
-            RenderHelpers.renderTexturedCuboid(poseStack, buffer, postTexture, packedLight, packedOverlay, 0f, postStart, 0f, 2 / 16f, postEnd, 2 / 16f);
-            RenderHelpers.renderTexturedCuboid(poseStack, buffer, postTexture, packedLight, packedOverlay, 0f, postStart, 1 - width, 2 / 16f, postEnd, 1);
+            drawCube(poseStack, buffer, postTexture, packedLight, packedOverlay, 0f, postStart, 0f, 2 / 16f, postEnd, 2 / 16f, 0, 0, 1, 1, normal);
+            drawCube(poseStack, buffer, postTexture, packedLight, packedOverlay, 0f, postStart, 1 - width, 2 / 16f, postEnd, 1, 0, 0, 1, 1, normal);
         }
         else
         {
-            RenderHelpers.renderTexturedCuboid(poseStack, buffer, postTexture, packedLight, packedOverlay, 0f, postStart, 0f, width, postEnd, 2 / 16f);
-            drawGlass(poseStack, buffer, packedLight, packedOverlay, side, topConnection, bottom, getXVertices(width, topOffset, 1 / 16f, 0.5f, downOffset, 1 / 16f, 0.5f, topOffset, 1f - width, downOffset), normal);
+            drawCube(poseStack, buffer, postTexture, packedLight, packedOverlay, 0f, postStart, 0f, width, postEnd, 2 / 16f, 0, 0, 1, 1, normal);
+            drawGlass(poseStack, buffer, packedLight, packedOverlay, side, topConnection, bottom, getPlaneVertices(width, topOffset, 1 / 16f, 0.5f, downOffset, 1 / 16f, 0.5f, topOffset, 1f - width, downOffset), normal);
         }
+    }
+
+    private void drawCube(PoseStack poseStack, VertexConsumer buffer, TextureAtlasSprite texture, int packedLight, int packedOverlay, float minX, float minY, float minZ, float maxX, float maxY, float maxZ, float u0, float v0, float u1, float v1, Vec3i normal)
+    {
+        RenderHelpers.renderTexturedQuads(poseStack, buffer, texture, packedLight, packedOverlay, getXVertices(minX, minY, minZ, maxX, maxY, maxZ), 16, 16, normal.getX(), 0, normal.getZ(), true);
+        RenderHelpers.renderTexturedQuads(poseStack, buffer, texture, packedLight, packedOverlay, getYVertices(minX, minY, minZ, maxX, maxY, maxZ), 16, 16, 0, 1, 0, true);
+        RenderHelpers.renderTexturedQuads(poseStack, buffer, texture, packedLight, packedOverlay, getZVertices(minX, minY, minZ, maxX, maxY, maxZ), 16, 16, normal.getZ(), 0, normal.getX(), true);
     }
 
     private void drawGlass(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, boolean side, boolean top, boolean bottom, float[][] vertices, Vec3i normal)
     {
         TextureAtlasSprite glass = selectGlassTexture(side, bottom, top);
-        RenderHelpers.renderTexturedQuads(poseStack, buffer, glass, packedLight, packedOverlay, vertices, 16, 16, normal.getX(), normal.getY(), normal.getZ(), true);
+        RenderHelpers.renderTexturedQuads(poseStack, buffer, glass, packedLight, packedOverlay, vertices, 16, 16, normal.getX(), normal.getY(), normal.getZ(), false);
     }
 
     private TextureAtlasSprite selectGlassTexture(boolean side, boolean bottom, boolean top)
@@ -203,37 +211,83 @@ public class GreenhousePanelWallBlockModel extends GreenhouseBlockModel.Baked
     }
 
     /**
-     * Modified version of {@link RenderHelpers#getXVertices(float, float, float, float, float, float)} that lets you set the UV
+     * Modified version of {@link RenderHelpers#getDiagonalPlaneVertices(float, float, float, float, float, float, float, float, float, float)} that includes data for the normal
      */
-    private static float[][] getXVertices(float minX, float minY, float minZ, float maxX, float maxY, float maxZ, float u0, float v0, float u1, float v1)
+    private static float[][] getPlaneVertices(float minX, float minY, float minZ, float maxX, float maxY, float maxZ, float u0, float v0, float u1, float v1)
     {
         return new float[][] {
-            {minX, minY, minZ, u1, v1, 1},
-            {maxX, minY, maxZ, u0, v1, 1},
-            {maxX, maxY, maxZ, u0, v0, 1},
-            {minX, maxY, minZ, u1, v0, 1},
+            {minX, minY, minZ, u1, v1, 1.0F},
+            {maxX, minY, maxZ, u0, v1, 1.0F},
+            {maxX, maxY, maxZ, u0, v0, 1.0F},
+            {minX, maxY, minZ, u1, v0, 1.0F},
 
-            {maxX, minY, maxZ, u0, v1, -1},
-            {minX, minY, minZ, u1, v1, -1},
-            {minX, maxY, minZ, u1, v0, -1},
-            {maxX, maxY, maxZ, u0, v0, -1}
+            {maxX, minY, maxZ, u0, v1, -1.0F},
+            {minX, minY, minZ, u1, v1, -1.0F},
+            {minX, maxY, minZ, u1, v0, -1.0F},
+            {maxX, maxY, maxZ, u0, v0, -1.0F}
         };
     }
 
     /**
-     * Modified version of {@link RenderHelpers#getZVertices(float, float, float, float, float, float)} that lets you set the UV
+     * Modified version of {@link RenderHelpers#getXVertices(float, float, float, float, float, float)} with custom UV mapping
      */
-    public static float[][] getZVertices(float minX, float minY, float minZ, float maxX, float maxY, float maxZ, float u0, float v0, float u1, float v1)
+    public static float[][] getXVertices(float minX, float minY, float minZ, float maxX, float maxY, float maxZ)
     {
+        float u0 = 1 - minZ;
+        float u1 = 1 - maxZ;
+        float v0 = 1 - minY;
+        float v1 = 1 - maxY;
         return new float[][] {
-            {maxX, minY, minZ, u0, v1, 1},
-            {minX, minY, minZ, u1, v1, 1},
-            {minX, maxY, minZ, u1, v0, 1},
-            {maxX, maxY, minZ, u0, v0, 1},
-            {minX, minY, maxZ, u1, v0, -1},
-            {maxX, minY, maxZ, u0, v0, -1},
-            {maxX, maxY, maxZ, u0, v1, -1},
-            {minX, maxY, maxZ, u1, v1, -1}
+            {minX, minY, minZ, u0, v1, 1.0F},
+            {minX, minY, maxZ, u1, v1, 1.0F},
+            {minX, maxY, maxZ, u1, v0, 1.0F},
+            {minX, maxY, minZ, u0, v0, 1.0F},
+            {maxX, minY, maxZ, u1, v0, -1.0F},
+            {maxX, minY, minZ, u0, v0, -1.0F},
+            {maxX, maxY, minZ, u0, v1, -1.0F},
+            {maxX, maxY, maxZ, u1, v1, -1.0F}
+        };
+    }
+
+    /**
+     * Modified version of {@link RenderHelpers#getYVertices(float, float, float, float, float, float)} with custom UV mapping
+     */
+    public static float[][] getYVertices(float minX, float minY, float minZ, float maxX, float maxY, float maxZ)
+    {
+        float u0 = 1 - minZ;
+        float u1 = 1 - maxZ;
+        float v0 = 1 - minX;
+        float v1 = 1 - maxX;
+        return new float[][] {
+            {minX, maxY, minZ, u0, v1, 1.0F},
+            {minX, maxY, maxZ, u1, v1, 1.0F},
+            {maxX, maxY, maxZ, u1, v0, 1.0F},
+            {maxX, maxY, minZ, u0, v0, 1.0F},
+            {minX, minY, maxZ, u1, v0, -1.0F},
+            {minX, minY, minZ, u0, v0, -1.0F},
+            {maxX, minY, minZ, u0, v1, -1.0F},
+            {maxX, minY, maxZ, u1, v1, -1.0F}
+        };
+    }
+
+    /**
+     * Modified version of {@link RenderHelpers#getZVertices(float, float, float, float, float, float)} with custom UV mapping
+     */
+    public static float[][] getZVertices(float minX, float minY, float minZ, float maxX, float maxY, float maxZ)
+    {
+        float u0 = 1 - maxX;
+        float u1 = 1 - minX;
+        float v0 = 1 - maxY;
+        float v1 = 1 - minY;
+        return new float[][] {
+            {maxX, minY, minZ, u0, v1, 1.0F},
+            {minX, minY, minZ, u1, v1, 1.0F},
+            {minX, maxY, minZ, u1, v0, 1.0F},
+            {maxX, maxY, minZ, u0, v0, 1.0F},
+            {minX, minY, maxZ, u1, v0, -1.0F},
+            {maxX, minY, maxZ, u0, v0, -1.0F},
+            {maxX, maxY, maxZ, u0, v1, -1.0F},
+            {minX, maxY, maxZ, u1, v1, -1.0F}
         };
     }
 
