85a80bff118f6764f091bce5d4ca8a5bf082686d
eerussianguy <eerussianguy@gmail.com>
2026-01-16 08:43:43 -0500

    fix advancements

--- resources/advancements.py
+++ resources/advancements.py
@@ -1,5 +1,5 @@
-from mcresources import ResourceManager, utils, advancements
-from mcresources.advancements import AdvancementCategory
+from mcresources import ResourceManager
+from mcresources.advancements import AdvancementCategory, inventory_changed
 from mcresources.type_definitions import Json
 
 from constants import *
@@ -28,8 +28,8 @@ def generate(rm: ResourceManager):
     story.advancement('jarring_station', icon('firmalife:jarring_station'), 'Industrial Cannery', 'Craft a jarring station.', 'jars', inventory_changed('firmalife:jarring_station'), frame='challenge')
     story.advancement('smoker', icon('tfc:food/venison'), 'Up in Smoke', 'Place some string for smoking.', 'root', placed_block('firmalife:wool_string'))
     story.advancement('dry', icon('firmalife:drying_mat'), 'Hydrophobic', 'Craft a drying mat.', 'root', inventory_changed('firmalife:drying_mat'))
-    story.advancement('oven', icon('firmalife:oven_bottom'), 'Bread Machine', 'Craft a top and bottom oven, and a chimney.', 'root', multiple(inventory_changed('firmalife:oven_bottom', name='ob'), inventory_changed('firmalife:oven_top', name='ot'), inventory_changed('firmalife:oven_chimney', name='oc')), requirements=[['ot'], ['oc'], ['ob']])
-    story.advancement('oven_hopper', icon('firmalife:oven_hopper'), 'Hop it', 'Craft an oven hopper.', 'oven', inventory_changed('firmalife:oven_hopper'))
+    story.advancement('oven', icon('firmalife:clay_oven_bottom'), 'Bread Machine', 'Craft a top and bottom oven, and a chimney.', 'root', multiple(inventory_changed('firmalife:clay_oven_bottom', name='ob'), inventory_changed('firmalife:clay_oven_top', name='ot'), inventory_changed('firmalife:clay_oven_chimney', name='oc')), requirements=[['ot'], ['oc'], ['ob']])
+    story.advancement('oven_hopper', icon('firmalife:clay_oven_hopper'), 'Hop it', 'Craft an oven hopper.', 'oven', inventory_changed('firmalife:clay_oven_hopper'))
     story.advancement('ashtray', icon('firmalife:ashtray'), 'Free Fertilizer', 'Craft an ashtray.', 'oven', inventory_changed('firmalife:ashtray'))
     story.advancement('vat', icon('firmalife:vat'), 'A Large Vat', 'Craft a vat.', 'oven', inventory_changed('firmalife:vat'))
     story.advancement('stovetop_pot', icon('tfc:ceramic/pot'), 'Stovetop Pot', 'Put a pot on a bottom oven.', 'oven', generic('firmalife:stovetop_pot', None))
@@ -46,32 +46,9 @@ def generate(rm: ResourceManager):
     story.advancement('pickled_egg', icon('firmalife:food/pickled_egg'), 'You put WHAT in this?', 'Craft a pickled egg.', 'root', inventory_changed('firmalife:food/pickled_egg'))
     story.advancement('bacon', icon('firmalife:food/cooked_bacon'), 'Sizzle', 'Cook some bacon', 'root', inventory_changed('firmalife:food/bacon'))
 
-def kill_mob(mob: str, other: Dict = None) -> Json:
-    return generic('minecraft:player_killed_entity', {'entity': [entity_predicate(mob, other)]})
-
-# if the predicate is (and usually it will be) an EntityPredicate.Composite, this should be inside an array.
-# EntityPredicate.Composite wraps a vanilla loot event in a trigger event
-def entity_predicate(mob: str, other: Dict = None) -> Json:
-    dic = {
-        'condition': 'minecraft:entity_properties',
-        'predicate': {'type': mob},  # can be a hashtag to refer to entity tags
-        'entity': 'this',
-    }
-    if other is not None:
-        dic.update(other)
-    return dic
-
-
-def consume_item(item: str, name: str = 'item_consumed') -> Json:
-    if isinstance(item, str) and name == 'item_consumed':
-        name = item.split(':')[1]
-    return generic('minecraft:consume_item', {'item': utils.item_predicate(item)}, name=name)
 
 def icon(name: str) -> Json:
-    return {'item': name}
-
-def biome(biome_name: str) -> Json:
-    return generic('minecraft:location', {'location': {'biome': 'tfc:%s' % biome_name}}, name=biome_name)
+    return {'id': name}
 
 def multiple(*conditions: Json) -> Json:
     merged = {}
@@ -81,18 +58,6 @@ def multiple(*conditions: Json) -> Json:
 
 def generic(trigger_type: str, conditions: Json, name: str = 'special_condition') -> Json:
     return {name: {'trigger': trigger_type, 'conditions': conditions}}
-
-def inventory_changed(item: str | Json, name: str = 'item_obtained') -> Json:
-    if isinstance(item, str) and name == 'item_obtained':
-        name = item.split(':')[1]
-    return {name: advancements.inventory_changed(item)}
-
-def item_use_on_block(block: str, item: str, name: str = 'item_use_on_block_condition'):
-    block_json = {'tag': block[1:]} if block[0] == '#' else {'blocks': [block]}
-    return {name: {'trigger': 'minecraft:item_used_on_block', 'conditions': {
-        'location': {'block': block_json},
-        'item': {'items': [item]}
-    }}}
 
 def placed_block(block: str, name: str = 'block_placed_condition') -> Json:
     return {name: {'trigger': 'minecraft:placed_block', 'conditions': {'location': [{'block': block, 'condition': 'minecraft:block_state_property'}]}}}
